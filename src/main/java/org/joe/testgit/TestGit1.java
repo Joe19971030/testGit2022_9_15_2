b55dea79f7ec6d77f6b037ae7c785ec7fda2f13f
joe <1550298508@qq.com>
2022-09-15 17:36:52 +0800

    testgit third commit

--- src/main/java/org/joe/testgit/TestGit1.java
+++ src/main/java/org/joe/testgit/TestGit1.java
@@ -4,5 +4,6 @@ public class TestGit1 {
     public static void main(String[] args) {
         System.out.println("test1");
         System.out.println("test2");
+        System.out.println("test3");
     }
 }
